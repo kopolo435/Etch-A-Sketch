aeefb06797c606954520e3d0dca1f06b26d4aed2
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-01-26 17:10:51 -0500

    Corrige orden de codigo y añade prompt
    El queryselectorAll debe estar despues de la creacion de los gridItems
    para que funcione correctamente.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gridContainer=document.getElementById("gridContainer");
 const newGridButton=document.querySelector("#newGrid");
-const itemList=document.querySelectorAll(".gridItem");
+
 
 for(let i=0;i<256;i++){ //Crea grid inicial de 16x16
     let gridItem=document.createElement("div");
@@ -9,9 +9,13 @@ for(let i=0;i<256;i++){ //Crea grid inicial de 16x16
     
 }
 
-
+const itemList=document.querySelectorAll(".gridItem");
 itemList.forEach(item => item.addEventListener('mouseover', () =>{ //Registra evento en el item y cambia su color
     item.classList.add("hoveredItem");
     console.log('hola');
 }))
 
+newGridButton.addEventListener('click',() =>{
+  let squareAmount=Number(window.prompt('Enter the number of squares per side of the new grid'));
+  let scuareWidth= 32/squareAmount; //32 es el tamaño en em del grid
+})

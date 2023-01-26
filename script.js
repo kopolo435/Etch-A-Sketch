72151eae84049f3880db02705f111355d307e354
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-01-26 16:56:10 -0500

    Añade variable del boton newGrid y organiza codigo
    Se mueven todas las declaraciones de variables al inicio y se agregan
    unos comentarios

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const gridContainer=document.getElementById("gridContainer");
+const newGridButton=document.querySelector("#newGrid");
+const itemList=document.querySelectorAll(".gridItem");
 
-for(let i=0;i<256;i++){
+for(let i=0;i<256;i++){ //Crea grid inicial de 16x16
     let gridItem=document.createElement("div");
     gridItem.classList.add("gridItem");
     gridContainer.appendChild(gridItem);
     
 }
-const itemList=document.querySelectorAll(".gridItem");
-itemList.forEach(item => item.addEventListener('mouseover', () =>{
+
+
+itemList.forEach(item => item.addEventListener('mouseover', () =>{ //Registra evento en el item y cambia su color
     item.classList.add("hoveredItem");
     console.log('hola');
 }))

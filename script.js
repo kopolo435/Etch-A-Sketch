e8cc315f206317ce28d547d985271b885cee18b4
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-01-26 16:43:34 -0500

    Añade codigo que cambia color de los items
    Esta funcionalidad hace que cuando el mouse pase por encima de un item
    del grid, su cambie su color

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ for(let i=0;i<256;i++){
     gridContainer.appendChild(gridItem);
     
 }
+const itemList=document.querySelectorAll(".gridItem");
+itemList.forEach(item => item.addEventListener('mouseover', () =>{
+    item.classList.add("hoveredItem");
+    console.log('hola');
+}))
 
